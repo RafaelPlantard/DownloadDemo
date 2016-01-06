f399972ab29d3bf782d10bd9f9e840ade7e55d62
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-01-06 15:37:19 -0200

    Initial Commit

---
+++ DownloadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadDemo
+//
+//  Created by Rafael Ferreira on 1/6/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
