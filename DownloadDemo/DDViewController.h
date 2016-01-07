70bf9a03496120eeb417b56bf3c8d95f8070460d
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-01-07 11:17:41 -0200

    Finished work to migrate the e-book code (2.0 of AFNetworking) for a valid version.

--- DownloadDemo/DDViewController.h
+++ DownloadDemo/DDViewController.h
@@ -11,5 +11,16 @@
 /*! @brief The controller for the first view. */
 @interface DDViewController : UIViewController
 
+/*! @brief The file url to download it. */
+@property (weak, nonatomic) IBOutlet UITextField *downloadURLField;
+
+/*! @brief Action to be done when I touch the DownloadButton. */
+- (IBAction)downloadFile:(id)sender;
+
+/*! @brief The reference for the progress bar UI component.*/
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
+
+/*! @brief The reference for the loading UI component. */
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loading;
 
 @end
--- DownloadDemo/DDViewController.m
+++ DownloadDemo/DDViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "DDViewController.h"
+#import "AFNetworking.h"
 
 @interface DDViewController ()
 
@@ -14,14 +15,65 @@
 
 @implementation DDViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (NSURL *)downloadSavePathFor:(NSString *)filename {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    NSURL *documentsDirectoryURL = [fileManager URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+    
+    return [documentsDirectoryURL URLByAppendingPathComponent:filename];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)showMessage:(NSString *)message {
+    UIAlertController *messageController = [UIAlertController alertControllerWithTitle:@"Download Demo" message:message preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *continueAction = [UIAlertAction actionWithTitle:@"continue" style:UIAlertActionStyleCancel handler:nil];
+    
+    [messageController addAction:continueAction];
+    
+    [self presentViewController:messageController animated:YES completion:nil];
 }
 
+- (AFURLSessionManager *)generateSessionManager {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    manager.securityPolicy.validatesDomainName = NO;
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    return manager;
+}
+
+- (IBAction)downloadFile:(id)sender {
+    NSURL *url = [NSURL URLWithString:_downloadURLField.text];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFURLSessionManager *manager = [self generateSessionManager];
+    
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull downloadProgress) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _progressView.progress = downloadProgress.fractionCompleted;
+        });
+    } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
+        return [self downloadSavePathFor:response.suggestedFilename];
+    } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+        [_loading stopAnimating];
+        
+        if (!error) {
+            NSLog(@"File downloaded to: %@", filePath);
+            [self showMessage:@"File download successfully."];
+        } else {
+            [self showMessage:[NSString stringWithFormat:@"Download error: %@", error.localizedDescription]];
+        }
+    }];
+    
+    _progressView.hidden = NO;
+    _progressView.progress = 0;
+    
+    [_loading startAnimating];
+    
+    
+    [downloadTask resume];
+}
 @end
